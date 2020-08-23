f7f77a7e7c11c21c53ecc9a3c0a856fb704e3580
Mathina <mathina.n@ticketmelon.com>
2020-08-24 02:11:30 +0700

    added content to project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
+import LogitruckVDO from '../media/Logitruck-VDO.mp4'
 
 export default class Project extends Component {
     render() {
         return (
             <Container>
                 <h1 className="title">Intelligent Truck Tracking System</h1>
-                <p className="description">Project I and II</p>
+                <p className="description">GUI on Raspberry Pi Touchscreen</p>
+                <video width="100%" height="auto" controls muted preload="auto">
+                    <source src={LogitruckVDO} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </Container>
         )
     }
